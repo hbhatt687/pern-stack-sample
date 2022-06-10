04dafaefee13cb627a630fdbc0924096a297e18c
Harsh Bhatt <hbhatt687@gmail.com>
2022-06-09 20:43:45 -0400

    (feature) create a RESTful API with Postgres in NodeJS

--- server/index.js
+++ server/index.js
@@ -5,16 +5,68 @@ const pool = require("./db");
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // req.body
 
 // routes
 // create a todo
+app.post("/todos", async (req, res) => {
+    try {
+        const { description } = req.body;
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *", [description]);
+
+        res.json(newTodo.rows[0]);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // get all todos
+app.get("/todos", async (req, res) => {
+    try {
+        const todos = await pool.query("SELECT * FROM todo");
+        res.json(todos.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
+// get a todo
+app.get("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id]);
+
+        res.json(todo.rows);
+    } catch (err) {
+        console.error(err.message);
+    }
+});
 
 // update a todo
+app.put("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { description } = req.body;
+
+        const updatedTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+
+        res.json("Todo was updated.");
+    } catch (error) {
+        console.error(error.message);
+    }
+});
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id]);
+
+        res.json("Todo was deleted.");
+    } catch (error) {
+        console.error(error.message);
+    }
+})
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
